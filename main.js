5e0aa93f12d59a2a9faf421ffeebc3f55e509419
VMVU-MSIGAME\許謝建興 <vmvuruvu@gmail.com>
2023-12-20 23:08:08 +0800

    build : init view-engine &view file

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const {engine} = require('express-handlebars')
 
+app.use(express.static('public'))
+app.engine('.hbs',engine({extname:'.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views','./views')
 
 
 app.get('/',(req,res) =>{
