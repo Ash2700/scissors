7dfbb0b438d438c3e2733fb96b6783738555aec4
VMVU-MSIGAME\許謝建興 <vmvuruvu@gmail.com>
2023-12-20 22:44:36 +0800

    build: express server

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
 
 
 app.get('/',(req,res) =>{
-  res.redirect('/scissors')
+  res.redirect("/scissors")
+})
+
+app.get('/scissors',(req,res)=>{
+  res.send('helld wolrd')
+})
+
+
+app.listen(port,()=>{
+  console.log(`server is started http://localhost:${port} `)
 })
