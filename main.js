a01afb84a28e27ab855b3d981df6a784d09e8f4b
VMVU-MSIGAME\許謝建興 <vmvuruvu@gmail.com>
2023-12-21 20:34:08 +0800

    check after to push

--- main.js
+++ main.js
@@ -1,90 +1,77 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const {engine} = require('express-handlebars')
+const { engine } = require('express-handlebars')
 const fs = require('fs')
 let data = loadData()
 
 app.use(express.static('public'))
-app.engine('.hbs',engine({extname:'.hbs'}))
+app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
-app.set('views','./views')
+app.set('views', './views')
 
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
   res.redirect("/scissors")
 })
 
-app.get('/scissors',(req,res)=>{
+app.get('/scissors', (req, res) => {
   res.render('index')
-  
 })
-//get link v
 
-//tans to short v
-//save data??? v
-// check rep date??/ v
-//send back data v
-app.get('/shortlink', (req,res) => {
+app.get('/shortlink', (req, res) => {
   const inputURL = req.query.inputURL
   const originShort = takeBackShortlink(inputURL).shortURL
   const randSting = isRepeat(inputURL) ? originShort : getRandWord()
-  if(!isRepeat(inputURL)){
-    saveData(inputURL,randSting)
+  if (!isRepeat(inputURL)) {
+    saveData(inputURL, randSting)
   }
-  res.render('return',{shortlink:`www.scissors.com/${randSting}`})
+  res.render('return', { shortlink: `www.scissors.com/${randSting}` })
 })
 
-app.get('/www.scissors.com/*',(req,res)=>{
+app.get('/www.scissors.com/*', (req, res) => {
   const shortName = req.params[0]
-  const result =takeBackShortlink(shortName).inputURL
+  const result = takeBackShortlink(shortName).inputURL
   res.redirect(result)
 })
 
-function copyButton(){
-  const shortURL =document.getElementById("shortURL")
-  navigator.clipboard.writeText(shortURL.innerText)
-  .then(()=>alert('copied'))
-  .catch((error)=>{console.error(error.message)})
-}
-
-function takeBackShortlink(string){
-  if(isRepeat(string)){
-    const result = data.find((item) =>{ return  Object.values(item).includes(string)})
-  return result
+function takeBackShortlink(string) {
+  if (isRepeat(string)) {
+    const result = data.find((item) => { return Object.values(item).includes(string) })
+    return result
   }
   return ''
 }
-function isRepeat(string){
-  const result = data.some((item)=>Object.values(item).includes(string) )
+function isRepeat(string) {
+  const result = data.some((item) => Object.values(item).includes(string))
   return result
 }
 
-function getRandWord(){
+function getRandWord() {
   const characters =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  let result =''
-  for(let i=0 ; i < 5 ; i++){
+  let result = ''
+  for (let i = 0; i < 5; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length))
   }
   return result
 }
 
-function saveData(inputURL,randSting){
-  data.push({inputURL, shortURL:`${randSting}`})
-  fs.writeFileSync('./public/jsons/data.json',JSON.stringify(data,null,2))
+function saveData(inputURL, randSting) {
+  data.push({ inputURL, shortURL: `${randSting}` })
+  fs.writeFileSync('./public/jsons/data.json', JSON.stringify(data, null, 2))
 }
 
-function loadData(){
-  try{
-    const jsonData = fs.readFileSync('./public/jsons/data.json','utf8')
+function loadData() {
+  try {
+    const jsonData = fs.readFileSync('./public/jsons/data.json', 'utf8')
     return JSON.parse(jsonData) || []
-  }catch(error){
-    console.error('讀取資料錯誤',error.message)
+  } catch (error) {
+    console.error('讀取資料錯誤', error.message)
     return []
   }
 }
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`server is started http://localhost:${port} `)
 })
--- test.js
+++
@@ -1,8 +0,0 @@
-const data = require('./public/jsons/data.json')
-
-console.log(data)
-
-data.forEach((item)=>{console.log(item)})
-
-const item = data.find((item) => {return Object.values(item).includes('Y13F6')  }).inputURL
-console.log(item)
